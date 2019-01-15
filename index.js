44be7291620d3941124d7dd5be7a0061be30baf4
s0ph1e <sophia@debitoor.com>
2019-01-15 15:33:43 +0200

    Bump version 0.1.1, allow multiple pages

--- index.js
+++ index.js
@@ -2,11 +2,10 @@ const puppeteer = require('puppeteer');
 
 class PuppeteerPlugin {
 	apply(registerAction) {
-		let browser, page;
+		let browser;
 
 		registerAction('beforeStart', async () => {
 			browser = await puppeteer.launch();
-			page = await browser.newPage();
 		});
 
 		registerAction('afterResponse', async ({response}) => {
@@ -14,8 +13,12 @@ class PuppeteerPlugin {
 			const isHtml = contentType && contentType.split(';')[0] === 'text/html';
 			if (isHtml) {
 				const url = response.request.href;
+
+				const page = await browser.newPage();
 				await page.goto(url);
-				return page.content();
+				const content = await page.content();
+				await page.close();
+				return content;
 			} else {
 				return response.body;
 			}
